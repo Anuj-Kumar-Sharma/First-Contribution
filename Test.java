e48c3e481aea7a7977e5b423208585cdda130828
pavan bhujbal <pavanbhujbal18@gmail.com>
2022-07-01 18:35:02 +0530

    Test added

---
+++ Test.java
@@ -0,0 +1,16 @@
+package session_01;
+
+public class Test {
+		
+	int data =10 ;		//instance variable
+	static int no = 555;// static variable
+	
+	public void myMethod()
+	{
+		int no = 1;		//local variable
+	}
+	
+	
+	
+	
+}
